9d01ed0ced841bc12dfbcc5abaf8da93baf6ff92
aainka <aainka@gmail.com>
2018-10-10 01:28:15 +0900

    SftpSync Refactoring

--- src/main/java/com/barolab/util/sftp/SftpSync.java
+++ src/main/java/com/barolab/util/sftp/SftpSync.java
@@ -37,9 +37,10 @@ public class SftpSync {
 
 	private JSch jsch = new JSch();
 	private Session session = null;
+	
+//	private OutputStream output = null;
+//	private InputStream input = null;
 	private Channel channel = null;
-	private OutputStream output = null;
-	private InputStream input = null;
 	public ChannelSftp sftpChannel;
 
 	public void testHost() {
