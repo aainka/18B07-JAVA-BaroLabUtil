d495c124b0beaa004603c1ea238a808d0a9fc498
aainka <aainka@gmail.com>
2018-12-14 19:22:38 +0900

    fun25

---
+++ src/main/java/com/barolab/HmTD.java
@@ -0,0 +1,61 @@
+package com.barolab;
+
+import java.util.Date;
+import java.util.List;
+
+import Platform.DashConsole.OV_Issue;
+
+public class HmTD extends HmObject {
+
+	private int width = 0;
+	private String align = null;
+
+	public void toHTML(HttpPrintStream h) {
+		String stag = new String("<td");
+		if ( width > 0 ) {
+			stag += " width="+width;
+		}
+		if ( align != null ) {
+			stag += " align=\""+align+"\"";
+		}
+		h.println(stag+" >");
+		for (Object anObject : comp) {
+			h.println(anObject);
+		}
+		h.println("</td>");
+
+	}
+
+	public void add(int value) {
+		comp.add("" + value);
+	}
+	
+	public void add(float value) {
+		comp.add("" + value);
+		
+	}
+
+	public void add(String sValue) {
+		comp.add(sValue);
+
+	}
+	public void add(Date spentOn) {
+		comp.add(spentOn.toLocaleString());
+		
+	}
+
+	public HmTD setWidth(int width) {
+		this.width = width;
+		return this;
+	}
+
+	public HmTD setAligh(String align) {
+		this.align = align;
+		return this;
+	}
+
+
+
+
+
+}
---
+++ src/main/java/com/barolab/HttpPrintStream.java
@@ -0,0 +1,46 @@
+package com.barolab;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class HttpPrintStream extends PrintStream {
+	
+	public HttpPrintStream(File arg0) throws FileNotFoundException {
+		super(arg0);
+		printMeta();
+	}
+	
+	private void printMeta() {
+		println("<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>");
+		println("<style>");
+		println(".mytable { border-collapse:collapse; }   ");
+		println(".mytable th, .mytable td { font-family:맑은 고딕; border:1px solid black; } ");
+		println("</style>");
+	}
+
+	File fp = new File("C:/tmp/test.html");
+	FileWriter fileWriter;
+
+	public void test() {
+		HtmlTable table = new HtmlTable();
+	//	HtmlTR tr = new HtmlTr();
+		try {
+			FileWriter fileWriter = new FileWriter(fp);
+			fileWriter.append("<html>\n");
+			fileWriter.append(String.format("<title>%s</title>\n", "hong"));
+			fileWriter.append("</html>\n");
+			fileWriter.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String arg[]) {
+
+	}
+
+}
